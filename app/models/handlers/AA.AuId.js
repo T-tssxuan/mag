79fd1048712a2b587a085b32ce59e9e922833e52
weilai <ijkcount@sina.com>
2016-05-06 19:55:05 +0800

    add AA.AuId handler

---
+++ app/models/handlers/AA.AuId.js
@@ -0,0 +1,107 @@
+var log4js = require('log4js');
+var async = require('async');
+var tadaRequest = require('../tada-request');
+var magUrlMake = require('../mag-url-make');
+
+var log = log4js.getLogger('Id-AA.AuId');
+
+// Using this object to check whether this path app suitable to the query pair
+var adatper = {
+    'Id': ['AA.AuId', 'Id'],
+    'AA.AuId': []
+}
+
+var limit = 1000;
+
+/**
+ * Search the path with given basePath
+ *
+ * @param {Object} reqInfo the infomation about the request
+ * @param {Object} reqDetail the infomation about the query pair
+ * @param {Array} basePath the base path of the request
+ * @param {Function} cbFunc the callback function
+ */
+function searchPath(reqInfo, reqDetail, result, basePath, cbFunc) {
+    async.parallel([
+        function(callback) {
+            // AA.AuId->AA.AfId->AA.AuId(AA.AuId)
+            if (reqDetail.desc[1] != 'AA.AuId') {
+                return callback(null);
+            }
+            var expr = "Composite(AA.AuId=" + reqDetail.value[1] + ")";
+            var url = magUrlMake(expr, "AA.AuId,AA.AfId", limit);
+            var afidElements = new Set();
+            // AA.AfId->AA.AuId
+            tadaRequest(url, reqInfo, function(err, data) {
+                if (!err) {
+                    for (var i = 0; i < data.length; i++) {
+                        for (var j = 0; j < data[i].AA.length; j++) {
+                            if (data[i].AA[j].AuId == reqDetail.value[1]) {
+                                afidElements.add(data[i].AA[j].AfId);
+                            }
+                        }
+                    }
+                    async.each(Array.from(afidElements), function(item, next) {
+                        var expr = "And(Id=" + reqDetail.value[0];
+                        expr += ",Composite(AA.AfId=" + item + "))";
+                        var url = magUrlMake(expr, "AA.AuId,AA.AfId", limit);
+                        tadaRequest(url, reqInfo, function(err, data) {
+                            if (!err && data.length > 0) {
+                                for (var i = 0; i < data[0].AA.length; i++) {
+                                    if (data[0].AA[i].AfId == item) {
+                                        result.push([reqDetail.value[0], 
+                                            data[0].AA[i].AuId, 
+                                            item, 
+                                            reqDetail.value[1]]);
+                                    }
+                                }
+                            }
+                            next(null);
+                        });
+                    }, function(err) {
+                        log.info("AA.AuId->AA.AfId->AA.AuId finished");
+                        callback(null);
+                    });
+                } else {
+                    log.info("AA.AuId->AA.AfId->AA.AuId error" + err);
+                    callback(null);
+                }
+            });
+        },
+        function(callback) {
+            // AA>AuId->Id->AA.AuId(AA.AuId)
+            if (reqDetail.desc[1] != 'AA.AuId') {
+                return callback(null);
+            }
+            callback(null);
+            
+        },
+        function(callback) {
+            // // AA>AuId->Id->RId(Id, RId)
+            if (reqDetail.desc[1] != 'Id') {
+                return callback(null);
+            }
+            callback(null);
+            
+        }
+    ], function(err) {
+        cbFunc(err);
+    });
+}
+
+/**
+ * Search the path with given basePath
+ *
+ * @param {Object} reqInfo the infomation about the request
+ * @param {Object} reqDetail the infomation about the query pair
+ * @param {Array} basePath the base path of the request
+ * @param {Function} cbFunc the callback function
+ */
+module.exports = function(reqInfo, reqDetail, result, basePath, cbFunc) {
+    // Before search path check whether this module suitable for query pair
+    if (adatper[reqDetail.desc[0]].indexOf(reqDetail.desc[1]) == -1) {
+        cbFunc();
+    } else {
+        searchPath(reqInfo, reqDetail, result, basePath, cbFunc);
+    }
+}
