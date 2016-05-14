3954bd4cdb9137290850e62aad0ad5876d9feaf3
bigbian <gybian@qq.com>
2016-05-14 18:01:31 +0800

    debug generate-afid-or-expr

--- app/models/generate-afid-or-expr.js
+++ app/models/generate-afid-or-expr.js
@@ -18,3 +18,30 @@ module.exports = function(AuIds, AfId)
     result.push(expr);
     return result;
 }
+
+function generateOrReq(AuIds, AfIds)
+{
+    var baseUrl = "http://oxfordhk.azure-api.net/academic/v1.0/evaluate?";
+    var magKey = "&subscription-key=f7cc29509a8443c5b3a5e56b0e38b5a6";
+    var SPACE = 2048 - baseUrl.length - magKey.length - 100;
+    var result = [];
+
+    var expr = '';
+    for (var i = 0; i < AuIds.length; i++) {
+        for (var j = 0; j < AfIds.length; j++) {
+            if (i == 0 && j == 0) {
+                expr = 'Composite(And(AA.AfId='+AfIds[j]+',AA.AuId='+AuIds[i]+'))';
+            } else {
+                if (expr.length + 50 < SPACE) {
+                    expr = 'Or(' + expr + ',';
+                    expr += 'Composite(And(AA.AfId=' + AfIds[j] + ',AA.AuId=' + AuIds[i] + ')))';
+                } else {
+                    result.push(expr);
+                    expr += 'Composite(And(AA.AfId=' + AfIds[j] + ',AA.AuId=' + AuIds[i] + '))';
+                }
+            }
+        }       
+    }
+    result.push(expr);
+    return result;
+}
